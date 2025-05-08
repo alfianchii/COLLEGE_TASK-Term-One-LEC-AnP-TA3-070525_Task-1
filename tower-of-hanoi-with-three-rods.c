1045c0fda76219ec434c3a6724b1b4042207de26
alfianchii <pianaee379@gmail.com>
2025-05-08 21:50:25 +0700

    feat: tower of hanoi (3 rods) algorithm

--- tower-of-hanoi-with-three-rods.c
+++ tower-of-hanoi-with-three-rods.c
@@ -6,15 +6,8 @@ void print_move(int start, int end) {
     printf("%d -> %d\n", start, end);
 }
 
-void hanoi(int n, int start, int end) {
-    if (n == 1) {
-        print_move(start, end);
-        return;
-    }
-}
-
 int sum_of_start_to_end(int start, int end) {
-    int sum = start;
+    int sum = 1;
 
     while (start < end)
     {
@@ -25,13 +18,23 @@ int sum_of_start_to_end(int start, int end) {
     return sum;
 }
 
+void hanoi(int n, int start, int end) {
+    if (n == 1) {
+        print_move(start, end);
+        return;
+    } else {
+        int sumStartToEnd = sum_of_start_to_end(START_ROD, END_ROD); // 6
+        int other = sumStartToEnd - (start + end); // 6 - 4 = 2
+        hanoi(n - 1, start, other); // 1, 1, 2
+        print_move(start, end); // 1 -> 3
+        hanoi(n - 1, other, end); // 1, 2, 3
+    }
+}
+
 int main() {
-    int amountOfDisk = 3;
+    int amountOfDisk = 2;
 
     hanoi(amountOfDisk, START_ROD, END_ROD);
-
-    int result = sum_of_start_to_end(START_ROD, END_ROD);
-    printf("The result of sum is: %d", result);
 
     return 0;
 }
