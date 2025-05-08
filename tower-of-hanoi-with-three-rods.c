8dcda9d33d8ecd03a1aaf8c6225c3f951e2e1c9a
alfianchii <pianaee379@gmail.com>
2025-05-08 21:37:41 +0700

    feat: sum the amount of start to end

--- tower-of-hanoi-with-three-rods.c
+++ tower-of-hanoi-with-three-rods.c
@@ -9,10 +9,25 @@ void hanoi(int n, int start, int end) {
     }
 }
 
+int sum_of_start_to_end(int start, int end) {
+    int sum = start;
+
+    while (start < end)
+    {
+        sum += end;
+        --end;
+    }
+
+    return sum;
+}
+
 int main() {
     int amountOfDisk = 1;
 
     hanoi(amountOfDisk, START_ROD, END_ROD);
-    
+
+    int result = sum_of_start_to_end(START_ROD, END_ROD);
+    printf("The result of sum is: %d", result);
+
     return 0;
 }
